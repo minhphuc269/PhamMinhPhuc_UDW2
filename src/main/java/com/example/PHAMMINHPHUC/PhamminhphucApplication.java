b0ba8381d4b40cf432a18af77f2938a1e7c54861
PhamMinhPhuc <miphucpl@gmail.com>
2025-03-03 07:56:21 +0700

    init project

---
+++ src/main/java/com/example/PHAMMINHPHUC/PhamminhphucApplication.java
@@ -0,0 +1,13 @@
+package com.example.PHAMMINHPHUC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamminhphucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamminhphucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PHAMMINHPHUC/PhamminhphucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PHAMMINHPHUC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamminhphucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
